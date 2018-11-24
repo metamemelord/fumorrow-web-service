e5972beb8f7e5a15dae4e24cd6ca5205c0e1db00
metamemelord <sainigaurav.2009@gmail.com>
2018-11-24 19:58:20 +0530

    Fixed a fatal issue with API

--- js/DAL/Commons/media/lib/AbstractUploadStrategy.js
+++ js/DAL/Commons/media/lib/AbstractUploadStrategy.js
@@ -4,7 +4,7 @@ const logger = require('../../../../Loggers').LoggerFactory.getLogger(filename);
 module.exports.upload = function (mediaDetails, callback) {
     switch (mediaDetails.type) {
         case 'image':
-            require('../images/upload')(mediaDetails, callback);
+            require('../images/Upload')(mediaDetails, callback);
             break;
         default:
             logger.error("Unknown type received");
