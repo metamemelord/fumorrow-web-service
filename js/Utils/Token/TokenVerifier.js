8471f48d172504069883e0731f893799ce8f85f2
Gaurav Saini <sainigaurav.2009@gmail.com>
2019-01-22 00:28:18 +0530

    Added invalid token message

--- js/Utils/Token/TokenVerifier.js
+++ js/Utils/Token/TokenVerifier.js
@@ -2,11 +2,19 @@ const decrypt = require("../../lib/Security/EncryptionDecryption").decrypt;
 module.exports = (req, res, next) => {
   const token = req.header("authorization");
   const decryptedToken = decrypt(token);
-  if (token === undefined || decryptedToken === null) {
+  if (token === undefined) {
     return res.status(403).json({
       status: {
         code: 403,
         message: "Unauthorized access"
+      },
+      data: null
+    });
+  } else if (decryptedToken === null) {
+    return res.status(403).json({
+      status: {
+        code: 403,
+        message: "Invalid token"
       },
       data: null
     });
