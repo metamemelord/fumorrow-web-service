4fd2ee1d5c227bb882e8823edd8c7c83153424e7
Gaurav Saini <sainigaurav.2009@gmail.com>
2019-01-22 00:12:42 +0530

    Added encryption for tokens

--- js/Utils/Token/TokenVerifier.js
+++ js/Utils/Token/TokenVerifier.js
@@ -1,15 +1,17 @@
+const decrypt = require("../../lib/Security/EncryptionDecryption").decrypt;
 module.exports = (req, res, next) => {
-    const token = req.header('authorization');
-    if (token === undefined) {
-        return res.status(403).json({
-            "status": {
-                "code": 403,
-                "message": "Unauthorized access"
-            },
-            "data": null
-        });
-    } else {
-        req.token = token;
-        next();
-    }
-}
+  const token = req.header("authorization");
+  const decryptedToken = decrypt(token);
+  if (token === undefined || decryptedToken === null) {
+    return res.status(403).json({
+      status: {
+        code: 403,
+        message: "Unauthorized access"
+      },
+      data: null
+    });
+  } else {
+    req.token = decryptedToken;
+    next();
+  }
+};
